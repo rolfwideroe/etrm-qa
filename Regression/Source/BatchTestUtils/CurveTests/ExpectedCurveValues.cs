d8a885a7319e0b9c74073b46ae66cd3f127f0800
Øistein Stemme <oeistein.stemme@bradyplc.com>
2019-01-07 10:28:56 +0100

    ELVIZ-14014 (2018.2) Fixed the regression test to work regardless of report date

--- Regression/Source/BatchTestUtils/CurveTests/ExpectedCurveValues.cs
+++ Regression/Source/BatchTestUtils/CurveTests/ExpectedCurveValues.cs
@@ -5,6 +5,7 @@
         public string ErrorMessage { get; set; }
         public ExpectedCurveProperties ExpectedProperties { get; set; }
         public ExpectedCurveValue[] ExpectedCurveValues { get; set; }
+        public double? ExpectedRepeatingCurveValue { get; set; }
         public ExpectedCurveVolatility ExpectedCurveVolatility { get; set; }
     }
 }
